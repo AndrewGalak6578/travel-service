2033136b5157b92d6b30c79eee61c89515c1f2fb
AndrewGalak6578 <andrew.galaktionov@inbox.ru>
2026-01-14 13:21:15 +0500

    autocheck

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    server: {
+        host: true,
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+            clientPort: 5173
+        },
+        watch: {
+            usePolling: true,
+            interval: 300
+        }
+    }
 })
